434c1ed2368efd557a5a43fcdfb69ae128c5b11b
Luqmus <135509819+LuQmu5@users.noreply.github.com>
2024-05-08 21:52:50 +0400

    Update Spawner.cs

--- Assets/Visitor/Spawner.cs
+++ Assets/Visitor/Spawner.cs
@@ -10,15 +10,30 @@ namespace Assets.Visitor
         [SerializeField] private float _spawnCooldown;
         [SerializeField] private List<Transform> _spawnPoints;
         [SerializeField] private EnemyFactory _enemyFactory;
-
+        [SerializeField] private int _maxEnemiesWeight = 100;
+ 
+        private Dictionary<EnemyType, int> _weightMap;
         private List<Enemy> _spawnedEnemies = new List<Enemy>();
+        private int _currentWeight;
 
         private Coroutine _spawn;
 
         public event Action<Enemy> Notified;
 
+        public void Init()
+        {
+            _weightMap = new Dictionary<EnemyType, int>()
+            {
+                [EnemyType.Human] = 10,
+                [EnemyType.Ork] = 15,
+                [EnemyType.Elf] = 12,
+                [EnemyType.Robot] = 30,
+            };
+        }
+        
         public void StartWork()
         {
+            _currentWeight = 0;
             StopWork();
 
             _spawn = StartCoroutine(Spawn());
@@ -35,17 +50,24 @@ namespace Assets.Visitor
             if (_spawnedEnemies.Count == 0)
                 return;
 
-            _spawnedEnemies[UnityEngine.Random.Range(0, _spawnedEnemies.Count)].Kill();
+            Enemy randomEnemy = _spawnedEnemies[UnityEngine.Random.Range(0, _spawnedEnemies.Count)];
+            randomEnemy.Kill();
+            _currentWeight -= _weightMap[randomEnemy.Type];
         }
 
         private IEnumerator Spawn()
         {
             while (true)
             {
-                Enemy enemy = _enemyFactory.Get((EnemyType)UnityEngine.Random.Range(0, Enum.GetValues(typeof(EnemyType)).Length));
+                if (_currentWeight >= _maxEnemiesWeight)
+                    yield continue;
+                
+                EnemyType randomType = (EnemyType)UnityEngine.Random.Range(0, Enum.GetValues(typeof(EnemyType)).Length);
+                Enemy enemy = _enemyFactory.Get(randomType);
                 enemy.MoveTo(_spawnPoints[UnityEngine.Random.Range(0, _spawnPoints.Count)].position);
                 enemy.Died += OnEnemyDied;
                 _spawnedEnemies.Add(enemy);
+                
                 yield return new WaitForSeconds(_spawnCooldown);
             }
         }
